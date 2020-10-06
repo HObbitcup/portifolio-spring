44409790b21a8ec999f2e272dd511f1c01ab8c84
HObbitcup <fabricante75@gmail.com>
2020-10-06 14:54:34 -0300

    O dia que deu certo

---
+++ src/main/java/br/com/codificando/respository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package br.com.codificando.respository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.codificando.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+}
